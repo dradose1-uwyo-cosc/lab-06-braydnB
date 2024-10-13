2332d49c0222a2b51f423209d1253b409d035ba4
braydnB <bballar7@uwyo.edu>
2024-10-13 23:39:23 +0000

    Finished Lab 6

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# [Name]
-## [Assignment #]
-## [Submission date: MM/DD/YY]
+# [Braydn Ballard]
+## [Lab 6]
+## [Submission date: 10/13/24]
 ## Worked with/sources 
 * example
 * example
--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -86,15 +86,53 @@ print(len(random_string)) # Print out the size for reference
 # Output: each letter and its corresponding occurrence in alphabetical order
 
 print("*"*75)
-# Output which letter occurred the most 
 
-most_occurred = ""
-least_occurred = ""
+chardict = {}
+for char in random_string:
+    if char in chardict:
+        chardict[char] += 1
+    else:
+        chardict[char] = 1
 
-print(f"The letter that occurred the most is {most_occurred}")
+#schardict = list(chardict.keys())
+schardict = []
+for j in chardict.keys():
+    schardict.append(j)
+schardict.sort()
+scd = {i: chardict[i] for i in schardict}
+print(scd) # scd is the sorted dictionary pairs
+for key, value in scd.items():
+    print(f"{key} occurs {value} times!")
+
+
+
+most_occurred = 0
+least_occurred = 1000
+modict ={}
+lodict = {}
+for k, v in scd.items():
+    if v > most_occurred:
+        most_occurred = v
+        modict.clear()
+        modict[k] = v
+    if v < least_occurred:
+        least_occurred = v
+        lodict.clear()
+        lodict[k] = v
+
+
+for u in modict.keys():
+    print(f"The letter that occurred the most is {u}")
 print("*"*75)
 # Output which letter occurred the least 
-print(f"The letter that occurred the most is {least_occurred}")
+for y in lodict.keys():
+    print(f"The letter that occurred the least is {y}")
 print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+ #/25 will give the percent of each i e 103/25 is 4.12%, which makes sense as 103/2500 is 4.12% or .0412.
+ #scd is the list from the start that has each pair and number so...
+for key1, value1 in scd.items():
+    pervalue = value1/round(len(random_string)/100, 2)
+    print(f"{key1} occurs {value1} times, which is {round(pervalue, 2)}% of the total random string.")
+print("You can also change the size of the list or add characters and it will still work :)")
